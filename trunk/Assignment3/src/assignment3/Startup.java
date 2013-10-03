12e56edb576e5b31ee007ad949136edd2d865064
kunanont93@gmail.com <kunanont93@gmail.com@c52c4618-f501-6494-2886-448b6bf33636>
2013-10-03 10:48:09 +0000

    Non  add Startup page

--- trunk/Assignment3/src/assignment3/Startup.java
+++ trunk/Assignment3/src/assignment3/Startup.java
@@ -4,9 +4,10 @@
  */
 package assignment3;
 
-import java.awt.Color;
-import java.awt.Font;
+import net.miginfocom.swing.MigLayout;
+import java.awt.*;
 import javax.swing.*;
+import javax.swing.border.Border;
 
 /**
  *
@@ -14,23 +15,25 @@ import javax.swing.*;
  */
 public class Startup extends JFrame{
     private JPanel pstart;
-    private JLabel lstart;
-    private JButton bstart;
+    private JLabel l1,l2;
+    private JButton b1;
     private JComboBox combo;
-    private JTextField text1,text2;
+    private JTextField text1;
     private Main main;
     public Startup(){
         
-        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-        setVisible(true);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);        
         setLocation(320,70);
         setSize(800,600);  
-        
+
         pstart = (JPanel)getContentPane();
-        pstart.setBackground(Color.white);
+        pstart.setLayout(new MigLayout("insets 50 0 0 0"));
+        pstart.setBackground(new Color(0xff,0xf0,0xa5));
+        
         addComponents();
         addListener();
         
+        setVisible(true);
         setTitle("Doctor-patients Scheduler");
     
     }
@@ -38,9 +41,26 @@ public class Startup extends JFrame{
     private void addComponents(){
     
         text1 = new JTextField("Create your profile");
-        text2 = new JTextField("Or choose exists name: ");
-        Font f = new Font("Arial",Font.BOLD,40);
+        l2 = new JLabel("Or choose exists name:");
+        l1 = new JLabel("Doctor-patients Scheduler");
+        b1 = new JButton("GO!");
         combo = new JComboBox();
+        combo.addItem("TEXXXXT");
+        Font f = new Font("Arial",Font.BOLD,23);
+        
+        
+        text1.setFont(new Font("Arial",Font.BOLD,40));
+        l1.setFont(f);
+        l2.setFont(f);
+        b1.setFont(f);
+        combo.setFont(new Font("Arial",Font.BOLD,20));
+        
+        pstart.add(l1,"gapright 30%, gapleft 30%, width 50%,wrap 150px");
+        pstart.add(text1,"gapright 20%, gapleft 20%, width 70%,wrap 120px");
+        pstart.add(l2,"gapleft 10%");
+        pstart.add(combo,"pos 350px 405px");              
+        
+        pstart.add(b1,"pos 650px 490px");
     }
     
     private void addListener(){
@@ -51,4 +71,5 @@ public class Startup extends JFrame{
     public void setMain(Object menu){
         this.main = main;
     }
+
 }
