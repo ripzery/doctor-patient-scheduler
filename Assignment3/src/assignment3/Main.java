981287aad18d3fc0f754d4d22e8ac1f59cd6ac84
ripzery@gmail.com <ripzery@gmail.com@c52c4618-f501-6494-2886-448b6bf33636>
2013-10-03 16:22:55 +0000

    1.Change color 2.Add AllergyBox

--- Assignment3/src/assignment3/Main.java
+++ Assignment3/src/assignment3/Main.java
@@ -19,19 +19,25 @@ public class Main extends JFrame {
     private static Startup startup;
     private int width=800,height=600;
     private String name;
-    private JPanel main;
-    private JLabel lhead;
+    private JPanel TitleBar,allergyBox,informationBox,updateBox;
+    private JLabel title,allergyTitle;
+    private JCheckBox[] allergy;
     
     public Main(String name){        
         this.setDoctorName(name);
+        this.setLayout(new MigLayout());
         
         addComponent();
         addListener();
         
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setBounds(320,70,width,height);
-        getContentPane().setBackground(new Color(0xff,0xf0,0xa5));
+        
+        getContentPane().setBackground(new Color(0x3e,0x60,0x6f));
+        
         setVisible(true);
+        
+        
     }
     
     public static void main(String args[]){       
@@ -39,16 +45,44 @@ public class Main extends JFrame {
     }
     private void addComponent(){
         Font f = new Font("Arial",Font.BOLD,36);        
-        main = new JPanel();
-        main.setLayout(new MigLayout());
-        lhead = new JLabel("Welcome to Dr. "+name+" 's scheduler");
-        lhead.setFont(f);
-        lhead.setForeground(new Color(0x8e,0x28,0x00));
+        TitleBar = new JPanel();
+        TitleBar.setLayout(new MigLayout());
+        title = new JLabel("Dr. "+name+" 's scheduler");
+        title.setFont(f);
+        title.setForeground(new Color(0xd1,0xdb,0xbd));
         
-        main.add(lhead,"gapleft 15%");
-        main.setOpaque(false);
+        TitleBar.add(title,"gapleft 50px");
+        TitleBar.setBackground(new Color(0xfc,0xff,0xf5));
+        TitleBar.setOpaque(false);
         
-        add(main);
+        this.add(TitleBar,"gapleft 20%,width 60%,gapright 20%,wrap 20px");
+        
+        /*
+         * Ending of TitleBar
+         */
+        
+        allergyBox = new JPanel(new MigLayout());
+        allergy = new JCheckBox[5];
+        allergy[0] = new JCheckBox("  A");allergy[0].setFont(f);allergy[0].setOpaque(false);
+        allergy[1] = new JCheckBox("  B");allergy[1].setFont(f);allergy[1].setOpaque(false);
+        allergy[2] = new JCheckBox("  C");allergy[2].setFont(f);allergy[2].setOpaque(false);
+        allergy[3] = new JCheckBox("  D");allergy[3].setFont(f);allergy[3].setOpaque(false);
+        allergy[4] = new JCheckBox("  E");allergy[4].setFont(f);allergy[4].setOpaque(false);
+        
+        allergyTitle = new JLabel("Patient's allergy");
+        allergyTitle.setFont(new Font("Arial",Font.CENTER_BASELINE,16));
+        
+        allergyBox.add(allergyTitle,"wrap 30px,center");
+        allergyBox.add(allergy[0],"gapleft 3%,wrap 30px");
+        allergyBox.add(allergy[1],"gapleft 3%,wrap 30px");
+        allergyBox.add(allergy[2],"gapleft 3%,wrap 30px");
+        allergyBox.add(allergy[3],"gapleft 3%,wrap 30px");
+        allergyBox.add(allergy[4],"gapleft 3%,wrap 30px");
+        
+        allergyBox.setBackground(new Color(0x91,0xaa,0x9d));
+        
+        this.add(allergyBox,"gapleft 1%");
+        
     }
     
     private void addListener(){
