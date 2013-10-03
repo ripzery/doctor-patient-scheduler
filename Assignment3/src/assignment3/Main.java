7002720e6352785f1d5224211cb48b01ed67cb24
ripzery@gmail.com <ripzery@gmail.com@c52c4618-f501-6494-2886-448b6bf33636>
2013-10-03 18:09:45 +0000

    1.add InformationBox (need more design) 2.add AllergyBox (need more design) 3.start adding UpdateBox

--- Assignment3/src/assignment3/Main.java
+++ Assignment3/src/assignment3/Main.java
@@ -19,9 +19,13 @@ public class Main extends JFrame {
     private static Startup startup;
     private int width=800,height=600;
     private String name;
+    private String[] time,daylist;
     private JPanel TitleBar,allergyBox,informationBox,updateBox;
-    private JLabel title,allergyTitle;
+    private JLabel title,allergyTitle,patientName,choosetimefrom,choosetimeto;
     private JCheckBox[] allergy;
+    private JTextField namefield;
+    private JComboBox start,end,day;
+    private JButton insert,clear,finish,change;
     
     public Main(String name){        
         this.setDoctorName(name);
@@ -55,7 +59,7 @@ public class Main extends JFrame {
         TitleBar.setBackground(new Color(0xfc,0xff,0xf5));
         TitleBar.setOpaque(false);
         
-        this.add(TitleBar,"gapleft 20%,width 60%,gapright 20%,wrap 20px");
+        this.add(TitleBar,"gapleft 20%,width 60%,gapright 20%,wrap 20px,id title");
         
         /*
          * Ending of TitleBar
@@ -81,7 +85,53 @@ public class Main extends JFrame {
         
         allergyBox.setBackground(new Color(0x91,0xaa,0x9d));
         
-        this.add(allergyBox,"gapleft 1%");
+        this.add(allergyBox,"gapleft 1%,id allergy");
+        
+        /*
+         * Ending of allergyBox
+         */
+        
+        informationBox = new JPanel(new MigLayout("fillx","[][grow][][grow]","nogrid"));
+        f = new Font("Arial",Font.BOLD,20);  
+        patientName = new JLabel("Patient name : ");
+        patientName.setFont(f);
+        namefield = new JTextField(30);
+        namefield.setFont(f);
+        choosetimefrom = new JLabel("Choose time : ");
+        choosetimefrom.setFont(f);
+        choosetimeto = new JLabel(" to");
+        choosetimeto.setFont(f);
+        time = new String[]{"08:00","08:30","09:00","09:30","10:00","10:30","11:00","11:30"
+                ,"12:00","12:30","13:00","13:30","14:00","14:30","15:00","15:30","16:00"};
+        start = new JComboBox(time);
+        end = new JComboBox(time);
+        daylist = new String[]{"Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"};
+        day = new JComboBox(daylist);
+        insert = new JButton("Insert");
+        clear = new JButton("Clear");
+        finish = new JButton("Finish");
+        change = new JButton("Change doctor");
+        
+        informationBox.add(patientName,"grow");
+        informationBox.add(namefield,"gapx unrel unrel,wrap 30px");
+        informationBox.add(choosetimefrom,"grow");
+        informationBox.add(start,"grow");
+        informationBox.add(choosetimeto,"grow");
+        informationBox.add(end,",grow,wrap 30px");
+        informationBox.add(insert,"grow");
+        informationBox.add(clear,"grow");
+        informationBox.add(finish,"grow");
+        informationBox.add(change,"grow");
+        
+        this.add(informationBox,"pos allergy.x2+10 allergy.y 550 allergy.y2,id information");
+        
+        /*
+         * Ending of informationBox
+         */
+        
+        updateBox = new JPanel();
+        
+        this.add(updateBox,"pos information.x2+10 information.y 770 information.y2");
         
     }
     
